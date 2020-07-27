9f585989248787d685551dfa05e570b4c135e972
ShruthiAnand30 <67490120+ShruthiAnand30@users.noreply.github.com>
2020-07-27 14:26:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -48,31 +48,29 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
  
   drawSprites();
  
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-	Matter.Body.setStatic(package,false); 
-	package.velocityY=-3;
-	bounceOff();
+	function keyPressed() { 
+		if (keyCode === DOWN_ARROW) { 
+			Matter.Body.setStatic(packageBody,false); 
+			bounceOff();
+		} 
+	}
+function bounceOff(helicopterSprite,package){
+    if (helicopterSprite.x - package.x < package.width/2 + helicopterSprite.width/2
+      && package.x - helicopterSprite.x < package.width/2 + helicopterSprite.width/2) {
+    helicopterSprite.velocityX = helicopterSprite.velocityX * (-1);
+    package.velocityX = package.velocityX * (-1);
   }
-}
-function bounceOff(object1,object2){
-    if (object1.x - object2.x < object2.width/2 + object1.width/2
-      && object2.x - object1.x < object2.width/2 + object1.width/2) {
-    object1.velocityX = object1.velocityX * (-1);
-    object2.velocityX = object2.velocityX * (-1);
-  }
-  if (object1.y - object2.y < object2.height/2 + object1.height/2
-    && object2.y - object1.y < object2.height/2 + object1.height/2){
-    object1.velocityY = object1.velocityY * (-1);
-    object2.velocityY = object2.velocityY * (-1);
+  if (helicopterSprite.y - package.y < package.height/2 + helicopterSprite.height/2
+    && package.y - helicopterSprite.y < package.height/2 + helicopterSprite.height/2){
+    helicopterSprite.velocityY = helicopterSprite.velocityY * (-1);
+    package.velocityY = package.velocityY * (-1);
   }
   }
 
